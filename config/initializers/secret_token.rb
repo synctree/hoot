eff51557791d6e36b96de26dadab17abc015971b
Masahji Stewart <masahji@synctree.com>
2012-05-21 11:03:24 -0700

    initial commit with devise and twitter bootstrap install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoot::Application.config.secret_token = '08da91f8159af8e46e372bf3542689da5444a21a15f5f60451fc062a4be50865caff6444f2cee258d8bb3745dd5194e0077a98b486a0e1f0677e5f27164ad555'
